8742b27090099ea50a8ee054c101f36fd7585146
ss49919201 <sakashin10291029@gmail.com>
2024-07-27 16:42:19 +0900

    CSV出力

---
+++ typescript/csv/main.ts
@@ -0,0 +1,35 @@
+import { generate } from "csv-generate";
+import { Stringifier, stringify } from "csv-stringify";
+import { stringify as stringifySync } from "csv-stringify/sync";
+import { writeFileSync } from "fs";
+
+const stringfier: Stringifier = stringify({
+  header: true,
+  columns: {
+    word: "単語",
+    sentence: "これは\n文章です。",
+  },
+});
+
+const stringfierSync = stringifySync(
+  [{ word: "単語", sentence: "これは\n文章です。" }],
+  {
+    header: true,
+  }
+);
+
+const exportToStdout = (stringfier: Stringifier) => {
+  generate({
+    length: 2,
+    seed: 1,
+  })
+    .pipe(stringfier)
+    .pipe(process.stdout);
+};
+
+const exportToFile = (path: string, data: string) => {
+  writeFileSync(path, data);
+};
+
+exportToStdout(stringfier);
+exportToFile("./output.csv", stringfierSync);
