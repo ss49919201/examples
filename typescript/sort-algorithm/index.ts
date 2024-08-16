a2090853b5c4c2c56675ca240e3b1062f55527a7
ss49919201 <sakashin10291029@gmail.com>
2024-08-16 11:20:18 +0900

    bubble sort

--- typescript/sort-algorithm/index.ts
+++ typescript/sort-algorithm/index.ts
@@ -23,9 +23,34 @@ function quickSort(array: Array<number>): Array<number> {
   return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-console.log(quickSort([2]));
-console.log(quickSort([2, 4]));
-console.log(quickSort([2, 4, 6]));
-console.log(quickSort([2, 4, 6, 8]));
-console.log(quickSort([5, 2, 4, 6, 8, 1]));
-console.log(quickSort([5, 2, 9, 2, 1, 4, 6, 8, 1]));
+function bubbleSort(array: Array<number>): Array<number> {
+  const result: typeof array = [];
+  let tmp: typeof array = array;
+  for (let i = 0; i < array.length; i++) {
+    let max = tmp[0];
+    let maxIdx = 0;
+    for (const [j, v] of tmp.entries()) {
+      if (max < v) {
+        maxIdx = j;
+        max = v;
+      }
+    }
+    result.push(max);
+    tmp = [...tmp.slice(0, maxIdx), ...tmp.slice(maxIdx + 1)];
+  }
+  return result.reverse();
+}
+
+// console.log(quickSort([2]));
+// console.log(quickSort([2, 4]));
+// console.log(quickSort([2, 4, 6]));
+// console.log(quickSort([2, 4, 6, 8]));
+// console.log(quickSort([5, 2, 4, 6, 8, 1]));
+// console.log(quickSort([5, 2, 9, 2, 1, 4, 6, 8, 1]));
+
+console.log(bubbleSort([2]));
+console.log(bubbleSort([2, 4]));
+console.log(bubbleSort([2, 4, 6]));
+console.log(bubbleSort([2, 4, 6, 8]));
+console.log(bubbleSort([5, 2, 4, 6, 8, 1]));
+console.log(bubbleSort([5, 2, 9, 2, 1, 4, 6, 8, 1]));
