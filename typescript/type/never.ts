3815890a307fe68ff9e40876c46eadfca05f1328
ss49919201 <sakashin10291029@gmail.com>
2024-08-20 17:44:48 +0900

    never

---
+++ typescript/type/never.ts
@@ -0,0 +1,22 @@
+type T = "a" | "b" | "c";
+
+const fn = (v: T) => {
+  switch (v) {
+    case "a": {
+      console.log("a");
+      break;
+    }
+    case "b": {
+      console.log("b");
+      break;
+    }
+    case "c": {
+      console.log("c");
+      break;
+    }
+    default: {
+      const unreachable: never = v;
+      throw new Error(`Unreachable: ${unreachable}`);
+    }
+  }
+};
