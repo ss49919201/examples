a37999076544a13cae0b1c524a07a07cb3c42071
ss49919201 <sakashin10291029@gmail.com>
2024-08-30 08:59:49 +0900

    iterable object

--- typescript/iterable-object/index.ts
+++ typescript/iterable-object/index.ts
@@ -15,3 +15,10 @@ const iterable = {
 for (const value of iterable) {
   console.log(value);
 }
+
+console.log([...iterable]);
+
+const [first, second, ...rest] = iterable;
+console.log(first, second, rest);
+
+Promise.all(iterable).then((values) => console.log(values));
