6e27efb406109049c4bee8924ad848a400f60e03
ss49919201 <sakashin10291029@gmail.com>
2024-09-12 23:16:22 +0900

    float

--- go/floating-point-number/main.go
+++ go/floating-point-number/main.go
@@ -1,20 +1,25 @@
 package main
 
 type bit struct {
-	value uint8
+	value uint8 // 0 or 1
 }
 
+// 10進数の少数点数には、2進数で表現できない数がある
 type float32 struct {
 	sign     bit
-	exponent [2]bit
+	exponent [8]bit
 	fraction [23]bit
 }
 
 func main() {
 	_ = float32{
 		sign:     bit{0}, // 0 for positive, 1 for negative
-		exponent: [2]bit{{0}, {0}},
-		fraction: [23]bit{{0}},
+		exponent: [8]bit{{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}},
+		fraction: [23]bit{
+			{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+			{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+			{0}, {0}, {0}, {0}, {0}, {0}, {0},
+		},
 	}
 
 }
