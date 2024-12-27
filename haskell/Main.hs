d1c921c48ab853ddc1ed25d2b9bf965701aa55d0
ss49919201 <sakashin10291029@gmail.com>
2024-12-28 07:15:22 +0900

    pattern match & guard

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,5 +1,13 @@
 -- logical operator
 logicalOperator = True && False || not False
 
+-- 引数の文字列が Japan の場合のみ True を返す。
+isJapan "Japan" = True
+isJapan x = False
+
+isJapan' x
+  | x == "Japan" = True
+  | otherwise = False
+
 main = do
   print logicalOperator -- True
