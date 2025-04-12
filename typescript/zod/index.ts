1731c96a9bc55aedd215f3487d617be69b9c5040
Shinya Sakae <ss49919201@gmail.com>
2025-04-12 12:54:59 +0900

    add jsonArrayToObj function to parse JSON array into structured object

--- typescript/zod/index.ts
+++ typescript/zod/index.ts
@@ -7,8 +7,6 @@ const User = z.object({
   blob: z.custom<Blob>(),
 });
 
-export type User = z.infer<typeof User>;
-
 const parse = <T>(schema: z.ZodType<T>, data: unknown): T => {
   try {
     return schema.parse(data);
@@ -31,3 +29,47 @@ const parsed2 = parse(User, {
   age: 30,
   extra: "extra",
 }); // Error: Invalid data
+
+const t1 = z.object({
+  type: z.literal("t1"),
+  id: z.string(),
+  nestKey: z.string(),
+});
+
+const t2 = z.object({
+  type: z.literal("t2"),
+  id: z.string(),
+  nestKey2: z.string(),
+});
+
+const oneOrTwo = z.union([t1, t2]);
+
+type Obj = {
+  key1: z.infer<typeof t1>[];
+  key2: z.infer<typeof t2>[];
+};
+
+function jsonArrayToObj(array: string[]): Obj {
+  const obj: Obj = {
+    key1: [],
+    key2: [],
+  };
+
+  for (const elem of array) {
+    const body = {
+      ...JSON.parse(elem),
+      id: "1",
+    };
+
+    const parsed = oneOrTwo.parse(body);
+    if (parsed.type === "t1") {
+      obj.key1.push(parsed);
+    } else if (parsed.type === "t2") {
+      obj.key2.push(parsed);
+    } else {
+      throw new Error("Invalid type");
+    }
+  }
+
+  return obj;
+}
