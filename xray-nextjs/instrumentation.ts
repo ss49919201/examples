a94d0bf0265f078b0969e975789dac6e91234c04
Shinya Sakae <ss49919201@gmail.com>
2025-11-03 07:41:57 +0900

    Call Lambda

--- xray-nextjs/instrumentation.ts
+++ xray-nextjs/instrumentation.ts
@@ -1,5 +1,15 @@
-import { registerOTel } from '@vercel/otel'
+import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel('xray-nextjs')
+  registerOTel({
+    serviceName: "xray-nextjs",
+    instrumentations: [
+      new HttpInstrumentation(),
+      new AwsInstrumentation({
+        suppressInternalInstrumentation: true,
+      }),
+    ],
+  });
 }
