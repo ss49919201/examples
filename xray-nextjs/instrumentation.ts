f5b308db50db0b2c90f2b4280936f49f12c51b75
Shinya Sakae <ss49919201@gmail.com>
2025-11-03 07:53:52 +0900

    propagate

--- xray-nextjs/instrumentation.ts
+++ xray-nextjs/instrumentation.ts
@@ -1,10 +1,12 @@
 import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import { AWSXRayPropagator } from "@opentelemetry/propagator-aws-xray";
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
   registerOTel({
     serviceName: "xray-nextjs",
+    propagators: [new AWSXRayPropagator()],
     instrumentations: [
       new HttpInstrumentation(),
       new AwsInstrumentation({
