bdb8e91fbc9d095e8859b26ab185131807a65318
Shinya Sakae <ss49919201@gmail.com>
2026-01-02 17:53:10 +0900

    implement lgo

--- typescript/nextjs-otel/app/page.tsx
+++ typescript/nextjs-otel/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  console.log({
+    params: await searchParams,
+  });
 
-export default function Home() {
+  if ((await searchParams)["throw"] === "true") {
+    throw new Error("throw error!");
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
