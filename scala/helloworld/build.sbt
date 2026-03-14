e4424dd969896e8906c6c082af3fb625dbd46218
Shinya Sakae <ss49919201@gmail.com>
2026-03-15 07:03:19 +0900

    add scala codes

--- scala/helloworld/build.sbt
+++
@@ -1,3 +0,0 @@
-scalaVersion := "3.8.1"
-
-scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked")
---
+++ scala/sandbox/src/main/scala/main.scala
@@ -0,0 +1,29 @@
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+@main
+def main(): Unit = {
+  val f: Future[String] = Future {
+    Thread.sleep(1000)
+    println("execute f")
+    "Wake up!"
+  }
+  val f2: Future[String] = Future {
+    Thread.sleep(100)
+    println("execute f2")
+    "Wake up!"
+  }
+
+  val composite = for {
+    a <- f
+    b <- f2
+  } yield (a, b)
+  composite onComplete {
+    case Success(s) => println(s)
+    case Failure(t) => println(t.getMessage)
+  }
+  Await.ready(f, 3000.seconds)
+  println("Done!")
+}
